77f380911f67fb1e94bf663f2d6e30043682a080
MATSUU Takuto <matsuu@gmail.com>
2022-11-06 20:25:06 +0900

    /api以外はブラウザキャッシュを有効に

--- src/server/index.js
+++ src/server/index.js
@@ -40,7 +40,9 @@ server.addHook("onRequest", async (req, res) => {
 });
 
 server.addHook("onRequest", async (req, res) => {
-  res.header("Cache-Control", "no-cache, no-store, no-transform");
+  if (req.url.match(/^\/api/)) {
+    res.header("Cache-Control", "no-cache, no-store, no-transform");
+  }
   res.header("Connection", "close");
 });
 
--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -9,6 +9,7 @@ export const spaRoute = async (fastify) => {
   fastify.register(fastifyStatic, {
     root: join(__dirname, "public"),
     wildcard: false,
+    maxAge: 60000,
     preCompressed: true,
   });
 
