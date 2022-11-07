1f50815e361bfdb6ff929b05c3e45a308a39f168
MATSUU Takuto <matsuu@gmail.com>
2022-11-07 20:39:07 +0900

    幅400の画像はサムネイル作成をcanvas方式に差し戻し

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -5,6 +5,31 @@ import React, { useEffect, useState } from "react";
  */
 
 /**
+ * @typedef Size
+ * @property {number} width
+ * @property {number} height
+ */
+
+/** @type {(cv: Size, img: Size) => Size} */
+const calcImageSize = (cv, img) => {
+  const constrainedHeight = cv.width * (img.height / img.width);
+
+  if (constrainedHeight >= cv.height) {
+    return {
+      height: constrainedHeight,
+      width: cv.width,
+    };
+  }
+
+  const constrainedWidth = cv.height * (img.width / img.height);
+
+  return {
+    height: cv.height,
+    width: constrainedWidth,
+  };
+};
+
+/**
  * @typedef Props
  * @property {string} src
  * @property {number} width
@@ -14,5 +39,36 @@ import React, { useEffect, useState } from "react";
 /** @type {React.VFC<Props>} */
 export const TrimmedImage = ({ height, src, width }) => {
   const imageUrl = src.replace(/(.*)\/([^\/]*)/, '$1/' + width + 'x' + height + '-$2');
-  return <img src={imageUrl} />;
+  if (width == 100) {
+    return <img height={height} src={imageUrl} width={width} />;
+  }
+
+  const [dataUrl, setDataUrl] = useState(null);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+
+      const size = calcImageSize(
+        { height: canvas.height, width: canvas.width },
+        { height: img.height, width: img.width },
+      );
+
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(
+        img,
+        -(size.width - canvas.width) / 2,
+        -(size.height - canvas.height) / 2,
+        size.width,
+        size.height,
+      );
+      setDataUrl(canvas.toDataURL());
+    };
+  }, [height, src, width]);
+
+  return <img src={dataUrl} />;
 };
