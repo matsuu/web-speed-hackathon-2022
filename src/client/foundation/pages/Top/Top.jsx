41228b48bece6049a3168ac9215a53893250741f
MATSUU Takuto <matsuu@gmail.com>
2022-11-10 20:22:46 +0900

    userDataが存在する場合のみChargeDialogを展開

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -153,7 +153,9 @@ export const Top = () => {
         )}
       </section>
 
-      <ChargeDialog ref={chargeDialogRef} onComplete={handleCompleteCharge} />
+      { userData && (
+        <ChargeDialog ref={chargeDialogRef} onComplete={handleCompleteCharge} />
+      )}
     </Container>
   );
 };
